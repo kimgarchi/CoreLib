688886b5b0a6784bb0bae74f9d129df0f0a42136
kimgarchi <kimgarchi@naver.com>
2019-09-01 20:35:12 +0900

    래핑 클래스 정의

--- Wrapper.h
+++ Wrapper.h
@@ -1,27 +1,92 @@
 #pragma once
 #include "stdafx.h"
-#include "ObjectManager.h"
 
-template<typename T>
-class Wrapper final
+namespace gc
 {
-public:
-	Wrapper<T>(const ObjectPool& bind_obj_pool);
-	~Wrapper();
+	template<typename T>
+	class unique_wrapper
+	{
+	public:
+		unique_wrapper(T* data)
+			: data_(data), use_count_(0)
+		{
+			add_use_count();
+		}
 
-	Wrapper(const Wrapper& wr_obj) = delete;
-	Wrapper& operator=(const Wrapper&) = delete;
-	
-private:
-	friend class Object;
+		unique_wrapper(unique_wrapper<T>& copy_wrapper)
+			: data_(copy_wrapper.data_), use_count_(0)
+		{
+			copy_wrapper.sub_use_count();
+			copy_wrapper.data_ = nullptr;
+			add_use_count();
+		}
 
-	using Owner = Object *;
-	using CallStack = Object *;	
-	using BackTracer = std::map<Owner, CallStack>;
+		virtual ~unique_wrapper()
+		{
+			sub_use_count();
 
-	bool Init();
+			if (use_count() <= 0)
+			{
+				delete data_;
+				data_ = nullptr;
+			}
+		}
 
-	Object* object_;
-	BackTracer back_tracer_;
-	const ObjectPool& bind_obj_pool_;
-};
+		T* operator()() { return data(); }
+		T& operator*() { return *data(); }
+		T& operator->() { return *data(); }
+
+		virtual unique_wrapper<T>& operator=(const unique_wrapper<T>&) = delete;
+
+	protected:
+		virtual T* data() { return data_; }
+		virtual std::atomic<WORD>& use_count() { return use_count_; }
+
+		void add_use_count() { use_count().fetch_add(1); }
+		void sub_use_count() { use_count().fetch_sub(1); }
+
+	private:
+		T* data_;
+		std::atomic<WORD> use_count_;
+	};
+
+	template<typename T>
+	class shared_wrapper final : public unique_wrapper<T>
+	{
+	public:
+		shared_wrapper(T* template_data)
+			: unique_wrapper<T>(template_data), orign_wrapper(nullptr)
+		{
+		}
+
+		shared_wrapper(shared_wrapper<T>& copy_wrapper)
+			: unique_wrapper<T>(nullptr), orign_wrapper(&copy_wrapper)
+		{
+			unique_wrapper<T>::add_use_count();
+		}
+
+		virtual ~shared_wrapper()
+		{
+			unique_wrapper<T>::sub_use_count();
+		}
+
+		virtual std::atomic<WORD>& use_count() override
+		{
+			if (orign_wrapper != nullptr)
+				return orign_wrapper->use_count();
+
+			return unique_wrapper<T>::use_count();
+		}
+
+	private:
+		virtual T* data() override
+		{
+			if (orign_wrapper != nullptr)
+				return orign_wrapper->data();
+
+			return reinterpret_cast<T*>(unique_wrapper<T>::data());
+		}
+
+		shared_wrapper<T>* orign_wrapper;
+	};
+}
