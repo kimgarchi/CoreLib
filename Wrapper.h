963ab0444900de143950ca5f41cdb73a4c51c13e
kimgarchi <kimgarchi@naver.com>
2019-09-04 01:19:31 +0900

    0903 작업

--- Wrapper.h
+++ Wrapper.h
@@ -9,13 +9,13 @@ namespace gc
 	{
 	public:
 		unique_wrapper(T* data);
-		unique_wrapper(unique_wrapper<T>& copy_wrapper) = delete;
-
 		virtual ~unique_wrapper();
 
 		T* operator()() { return data(); }
 		T& operator*() { return *data(); }
 		T& operator->() { return *data(); }
+
+		virtual bool expired() { return (data_ != nullptr && use_count() > 0); }
 
 	protected:
 		virtual std::atomic<WORD>& use_count() { return strong_use_count_; }
@@ -28,28 +28,45 @@ namespace gc
 		T* data_;
 
 	private:
-		
 		std::atomic<WORD> strong_use_count_;
 	};
 
 	template<typename T>
-	class shared_wrapper final : public unique_wrapper<T>
+	class shared_wrapper : public unique_wrapper<T>
 	{
 	public:
 		shared_wrapper(T* template_data);
 		shared_wrapper(shared_wrapper<T>& copy_wrapper);
-		shared_wrapper(unique_wrapper<T>& copy_wrapper);
 
 		virtual ~shared_wrapper();
 		virtual std::atomic<WORD>& use_count() override;
 
-		virtual void operator=(unique_wrapper<T>& orgin_wrapper);
+		virtual void operator=(shared_wrapper<T>&) = delete;
+		virtual void operator=(unique_wrapper<T>& orgin_wrapper) = delete;
+
+		virtual bool expired() override;
+
+	protected:		
+		std::atomic<WORD>& weak_use_count();		
+		void add_weak_use_count() { weak_use_count_.fetch_add(1); }
+		void add_weak_use_count() { weak_use_count_.fetch_sub(1); }
 
 	private:
 		virtual T* data() override;
 
-		shared_wrapper<T>* orign_wrapper;
 		std::atomic<WORD> weak_use_count_;
+		shared_wrapper<T>* orign_wrapper_;
+	};
+
+	template<typename T>
+	class weak_wrapper
+	{
+	public:
+		weak_wrapper(shared_wrapper<T>& origin_wrapper);
+		virtual ~weak_wrapper();
+	private:
+
+
 	};
 }
 
@@ -74,52 +91,56 @@ namespace gc
 	// shared
 	template<typename T>
 	shared_wrapper<T>::shared_wrapper(T* template_data)
-		: unique_wrapper<T>(template_data), orign_wrapper(nullptr), weak_use_count_(0)
+		: unique_wrapper<T>(template_data), orign_wrapper_(nullptr)
 	{
-		unique_wrapper<T>::add_use_count();
+		this->add_use_count();
 	}
 
 	template<typename T>
 	shared_wrapper<T>::shared_wrapper(shared_wrapper<T>& copy_wrapper)
-		: unique_wrapper<T>(nullptr), orign_wrapper(&copy_wrapper), weak_use_count_(0)
+		: unique_wrapper<T>(nullptr), orign_wrapper_(&copy_wrapper)
 	{
-		unique_wrapper<T>::add_use_count();
-	}
-
-	template<typename T>
-	shared_wrapper<T>::shared_wrapper(unique_wrapper<T>& copy_wrapper)
-		: unique_wrapper<T>(copy_wrapper.data_), orign_wrapper(nullptr), weak_use_count_(0)
-	{
-		copy_wrapper.data_ = nullptr;
+		this->add_use_count();
 	}
 
 	template<typename T>
 	shared_wrapper<T>::~shared_wrapper()
 	{
-		unique_wrapper<T>::sub_use_count();
+		this->sub_use_count();
 	}
 
 	template<typename T>
 	std::atomic<WORD>& shared_wrapper<T>::use_count()
 	{
-		if (orign_wrapper != nullptr)
-			return orign_wrapper->use_count();
+		if (orign_wrapper_ != nullptr)
+			return orign_wrapper_->use_count();
 
 		return unique_wrapper<T>::use_count();
 	}
 
 	template<typename T>
-	void shared_wrapper<T>::operator=(unique_wrapper<T>& orgin_wrapper)
+	inline bool shared_wrapper<T>::expired()
 	{
-		this->data = orign_wrapper.data_;
-		orign_wrapper.data_ = nullptr;
+		if (weak_use_count() == 0 && use_count() == 0)
+			return true;
+
+		return false;
+	}
+
+	template<typename T>
+	std::atomic<WORD>& shared_wrapper<T>::weak_use_count()
+	{	
+		if (orign_wrapper_ != nullptr)
+			return orign_wrapper_->weak_use_count();
+
+		return weak_use_count_;		
 	}
 
 	template<typename T>
 	T* shared_wrapper<T>::data()
 	{
-		if (orign_wrapper != nullptr)
-			return orign_wrapper->data();
+		if (orign_wrapper_ != nullptr)
+			return orign_wrapper_->data();
 
 		return reinterpret_cast<T*>(unique_wrapper<T>::data());
 	}
